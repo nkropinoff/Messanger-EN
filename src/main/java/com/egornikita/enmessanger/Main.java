614fe11f571a8a646dc2fc63bb0a45bb66597c10
nkropinoff <nikkropinoff@gmail.com>
2024-12-07 09:49:53 +0300

    kropinov nikita - create printInfoAboutUsers method in Main class

--- src/main/java/com/egornikita/enmessanger/Main.java
+++ src/main/java/com/egornikita/enmessanger/Main.java
@@ -1,5 +1,15 @@
 package com.egornikita.enmessanger;
+import com.egornikita.enmessanger.model.*;
+
 
 public class Main {
+    public static void main(String[] args) {
 
+    }
+
+    public static void printInfoAboutUsers(User[] users) {
+        for (User u : users) {
+            System.out.printf("NickName: %s\nUserName: %s\nPhoneNumber: %s\nPassword: ********\n\n", u.getNickName(), u.getUserName(), u.getPhoneNumber());
+        }
+    }
 }
