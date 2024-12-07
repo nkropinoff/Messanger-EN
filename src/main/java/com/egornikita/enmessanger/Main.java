417f7b2469100e81372d939428db184ca0f07174
egor_sorokin <sorokine329@gmail.com>
2024-12-07 09:56:07 +0300

    sorokin egor создание метода printInfoAboutMessages

--- src/main/java/com/egornikita/enmessanger/Main.java
+++ src/main/java/com/egornikita/enmessanger/Main.java
@@ -12,4 +12,14 @@ public class Main {
             System.out.printf("NickName: %s\nUserName: %s\nPhoneNumber: %s\nPassword: ********\n\n", u.getNickName(), u.getUserName(), u.getPhoneNumber());
         }
     }
+
+    public static void printInfoAboutMessages(Message[] messages) {
+        for (Message message : messages) {
+            System.out.println("Sender: " + message.getSender().getNickName());
+            System.out.println("Receiver: " + message.getReceiver().getNickName());
+            System.out.println("Text: " + message.getText());
+            System.out.println("Time: " + message.getTime());
+            System.out.println();
+        }
+    }
 }
