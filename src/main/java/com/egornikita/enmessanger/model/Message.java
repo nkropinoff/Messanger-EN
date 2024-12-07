1d7d59ddf3d4cb1020271912e44de372ddc8b24d
egor_sorokin <sorokine329@gmail.com>
2024-12-07 09:11:34 +0300

    egor sorokin создание класса Message

--- src/main/java/com/egornikita/enmessanger/model/Message.java
+++ src/main/java/com/egornikita/enmessanger/model/Message.java
@@ -1,4 +1,56 @@
 package com.egornikita.enmessanger.model;
 
+//- Сообщение
+//    - Отправитель (пользователь)
+//    - Получатель  (пользователь)
+//    - Текст
+//    - Дата-время
+
 public class Message {
+    private User sender;
+    private User receiver;
+    private String text;
+    private String time;
+
+    public Message(User sender, User receiver, String text, String time) {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.text = text;
+        this.time = time;
+    }
+
+    public Message() {
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public User getReceiver() {
+        return receiver;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public void setReceiver(User receiver) {
+        this.receiver = receiver;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
