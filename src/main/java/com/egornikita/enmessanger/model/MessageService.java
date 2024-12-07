b30c79aac2e896fde272ddcf757811dce692cd2b
egor_sorokin <sorokine329@gmail.com>
2024-12-07 09:41:38 +0300

    egor sorokin создание класса MessageService.java с методом create4Message

--- src/main/java/com/egornikita/enmessanger/model/MessageService.java
+++ src/main/java/com/egornikita/enmessanger/model/MessageService.java
@@ -1,4 +1,13 @@
 package com.egornikita.enmessanger.model;
 
 public class MessageService {
+    public static Message[] create4Message(User[] users) {
+        Message[] messages = new Message[users.length];
+
+        for (int i = 0; i < users.length; i++) {
+            messages[i] = new Message(users[i], users[(i + 1) % users.length], "Привет, " + users[(i + 1) % users.length].getNickName() + ", приятно познакомиться!", 12 + i + ":05");
+        }
+
+        return messages;
+    }
 }
